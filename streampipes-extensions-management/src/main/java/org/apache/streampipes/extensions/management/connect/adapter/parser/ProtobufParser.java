f23b688d499eaba659e486fd8e34888ec8a51482
Jason Hao <jasonhao518@gmail.com>
2025-05-04 10:34:01 +0200

    Update ProtobufParser.java

--- streampipes-extensions-management/src/main/java/org/apache/streampipes/extensions/management/connect/adapter/parser/ProtobufParser.java
+++ streampipes-extensions-management/src/main/java/org/apache/streampipes/extensions/management/connect/adapter/parser/ProtobufParser.java
@@ -176,10 +176,9 @@ public class ProtobufParser implements IParser {
         return toMap(((DynamicMessage) value).getAllFields());
     }else if ( value instanceof ByteString) {
       ByteBuffer buffer = ((ByteString)value).asReadOnlyByteBuffer();
-      buffer.order(ByteOrder.LITTLE_ENDIAN); // Or BIG_ENDIAN depending on C side
-      List<Short> shorts = new ArrayList<>();
+      List<Byte> shorts = new ArrayList<>();
       while(buffer.hasRemaining()) {
-        shorts.add(buffer.getShort());
+        shorts.add(buffer.get());
       }
       return shorts;
     } else if (value instanceof List) {
